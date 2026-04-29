c8a63034dec96aee6d325333cd18303da3d25c99
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-29 21:58:34 +0530

    project

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -6,3 +6,11 @@ variable "environment" {
     default = "dev"
 }
 
+variable "sg_names" {
+    default = [
+        # databases
+        "mongodb", "redis", "mysql", "rabbitmq",
+        # backend
+        # "catalogue", "user", "cart", "shipping", "payment",
+        # frontend
+        # "frontend",
