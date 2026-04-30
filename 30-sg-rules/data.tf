e6d0a5a78f145d54e50bf4d68d6e5293dcea948e
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-30 22:56:55 +0530

    project

--- 30-sg-rules/data.tf
+++ 30-sg-rules/data.tf
@@ -5,3 +5,13 @@
 data "aws_ssm_parameter" "ingress_alb_sg_id" {
   name = "/${var.project_name}/${var.environment}/ingress_alb_sg_id"
 }
+
+data "aws_ssm_parameter" "bastion_sg_id" {
+  name = "/${var.project_name}/${var.environment}/bastion_sg_id"
+}
+data "aws_ssm_parameter" "mongodb_sg_id" {
+  name = "/${var.project_name}/${var.environment}/mongodb_sg_id"
+}
+
+
+
