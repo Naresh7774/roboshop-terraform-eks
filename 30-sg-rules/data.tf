cf40a1fc180c326c2c8736849d94aa173c478d36
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-30 22:58:02 +0530

    project

--- 30-sg-rules/data.tf
+++ 30-sg-rules/data.tf
@@ -41,6 +41,13 @@ data "aws_ssm_parameter" "mysql_sg_id" {
 #   name = "/${var.project_name}/${var.environment}/shipping_sg_id"
 # }
 
+# data "aws_ssm_parameter" "payment_sg_id" {
+#   name = "/${var.project_name}/${var.environment}/payment_sg_id"
+# }
+
+# data "aws_ssm_parameter" "frontend_sg_id" {
+#   name = "/${var.project_name}/${var.environment}/frontend_sg_id"
+# }
 
 
 
