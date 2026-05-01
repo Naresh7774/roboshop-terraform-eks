904f314520444c5e4e452ececbd149b0e3b04691
Narii Chowdary <nareshchowdary164@gmail.com>
2026-05-01 21:34:10 +0530

    project

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -83,3 +83,13 @@ resource "aws_security_group_rule" "mysql_bastion" {
 #   protocol          = "tcp"
 #   to_port           = 3306
 # }
+
+
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+  type              = "ingress"
+  security_group_id = local.rabbitmq_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
