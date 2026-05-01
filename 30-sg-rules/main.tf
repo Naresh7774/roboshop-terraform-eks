23926eed4aeb5a079ad0017a96c1a2abe485845b
Narii Chowdary <nareshchowdary164@gmail.com>
2026-05-01 21:33:15 +0530

    project

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -14,3 +14,14 @@ resource "aws_security_group_rule" "mongodb_bastion" {
 #   protocol          = "tcp"
 #   to_port           = 27017
 # }
+
+
+
+# resource "aws_security_group_rule" "mongodb_user" {
+#   type              = "ingress"
+#   security_group_id = local.mongodb_sg_id
+#   source_security_group_id = local.user_sg_id
+#   from_port         = 27017
+#   protocol          = "tcp"
+#   to_port           = 27017
+# }
