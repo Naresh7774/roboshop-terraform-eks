ac5a7dec2df7a672e7c3e7daaaf88d4cde17fe00
Narii Chowdary <nareshchowdary164@gmail.com>
2026-05-02 22:14:12 +0530

    project

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -195,3 +195,13 @@ resource "aws_security_group_rule" "shipping_backend_alb" {
   protocol          = "tcp"
   to_port           = 8080
 }
+
+##### Payment SG Rules #####
+resource "aws_security_group_rule" "payment_bastion" {
+  type              = "ingress"
+  security_group_id = local.payment_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
