476c87a315fa26fd42e05b570464b221000848d2
Narii Chowdary <nareshchowdary164@gmail.com>
2026-05-02 22:19:44 +0530

    project

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -316,3 +316,13 @@ resource "aws_security_group_rule" "bastion_laptop" {
   protocol          = "tcp"
   to_port           = 22
 }
+
+
+resource "aws_security_group_rule" "open_vpn_public" {
+  type              = "ingress"
+  security_group_id = local.open_vpn_sg_id
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
