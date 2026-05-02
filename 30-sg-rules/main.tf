a6377e3f5dc803e03c658766f7021e48d6ee8931
Narii Chowdary <nareshchowdary164@gmail.com>
2026-05-02 22:14:46 +0530

    project

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -233,3 +233,13 @@ resource "aws_security_group_rule" "backend_alb_bastion" {
   protocol          = "tcp"
   to_port           = 80
 }
+
+
+resource "aws_security_group_rule" "backend_alb_frontend" {
+  type              = "ingress"
+  security_group_id = local.backend_alb_sg_id
+  source_security_group_id = local.frontend_sg_id
+  from_port         = 80
+  protocol          = "tcp"
+  to_port           = 80
+}
