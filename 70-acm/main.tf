ee3b8dfbfa7ca6cae27c43abf9ef69d996282462
Narii Chowdary <nareshchowdary164@gmail.com>
2026-05-03 16:10:07 +0530

    project

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -23,3 +23,11 @@ resource "aws_route53_record" "roboshop" {
     }
   }
 
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 1
+  type            = each.value.type
+  zone_id         = var.zone_id
+}
+
