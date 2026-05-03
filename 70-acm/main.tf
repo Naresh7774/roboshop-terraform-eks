cb5539cee6f2595e65bcd6b0fcfb2bbc51e2d0a8
Narii Chowdary <nareshchowdary164@gmail.com>
2026-05-03 16:09:40 +0530

    project

---
+++ 70-acm/main.tf
@@ -0,0 +1,11 @@
+resource "aws_acm_certificate" "roboshop" {
+  domain_name       = "*.${var.domain_name}"
+  validation_method = "DNS"
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = local.common_name_suffix
+    }
+  )
+
