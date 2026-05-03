19118c21a0083f11dcd2b1c114e42b239185ede5
Narii Chowdary <nareshchowdary164@gmail.com>
2026-05-03 16:07:28 +0530

    project

---
+++ 40-ecr/main.tf
@@ -0,0 +1,10 @@
+resource "aws_ecr_repository" "catalogue" {
+  name                 = "${var.project_name}/catalogue"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+  force_delete = true
+}
+
