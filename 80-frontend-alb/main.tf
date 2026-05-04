a050dc08e025126a4a82a4ea7798ab15867d2ab4
Narii Chowdary <nareshchowdary164@gmail.com>
2026-05-04 22:43:19 +0530

    project

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -9,3 +9,17 @@ resource "aws_acm_certificate" "roboshop" {
     }
   )
 
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "roboshop" {
+  for_each = {
+    for dvo in aws_acm_certificate.roboshop.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
