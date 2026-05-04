caee1e9a685e1f1e6218e06ac4150ec6a115e62f
Narii Chowdary <nareshchowdary164@gmail.com>
2026-05-04 22:44:33 +0530

    project

--- 80-frontend-alb/variables.tf
+++ 80-frontend-alb/variables.tf
@@ -6,10 +6,10 @@ variable "environment" {
     default = "dev"
 }
 
-variable "domain_name" {
-    default = "daws86s.fun"
-}
-
 variable "zone_id" {
     default = "Z0948150OFPSYTNVYZOY"
+}
+
+variable "domain_name" {
+    default = "daws86s.fun"
 }
