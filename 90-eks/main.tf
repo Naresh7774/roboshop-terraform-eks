5134b1978860fe4c39bda600dd9f7354e042c813
Narii Chowdary <nareshchowdary164@gmail.com>
2026-05-05 21:22:23 +0530

    project

---
+++ 90-eks/main.tf
@@ -0,0 +1,7 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0" # this is module version
+
+  name               = local.common_name_suffix
+  # kubernetes_version = "1.33"
+  kubernetes_version = var.eks_version
