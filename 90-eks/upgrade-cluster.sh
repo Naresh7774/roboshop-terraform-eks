1df362ab4a0bbd8cb6a2b83027e2539afc9542e8
Narii Chowdary <nareshchowdary164@gmail.com>
2026-05-06 23:59:28 +0530

    project

--- 90-eks/upgrade-cluster.sh
+++ 90-eks/upgrade-cluster.sh
@@ -13,3 +13,10 @@ N="\e[0m"
 #########
 CLUSTER_NAME="roboshop-dev"
 AWS_REGION="us-east-1"
+
+EKS_TARGET_VERSION=$1
+
+LOGS_FOLDER="/home/ec2-user/eks-upgrade"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
