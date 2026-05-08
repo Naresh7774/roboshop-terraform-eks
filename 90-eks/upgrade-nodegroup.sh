6aa34e37f0ac73a658d9c4f53aa88fd0979258c5
Narii Chowdary <nareshchowdary164@gmail.com>
2026-05-08 23:10:37 +0530

    project

--- 90-eks/upgrade-nodegroup.sh
+++ 90-eks/upgrade-nodegroup.sh
@@ -15,3 +15,16 @@ TARGET_NG_VERSION=""
 LOGS_FOLDER="/home/ec2-user/eks-upgrade"
 SCRIPT_NAME=$(echo "$0" | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+
+mkdir -p "$LOGS_FOLDER"
+echo "Script started executed at: $(date)" | tee -a "$LOG_FILE"
+
+VALIDATE(){
+  if [ $1 -ne 0 ]; then
+    echo -e "$2 ... $R FAILURE $N" | tee -a "$LOG_FILE"
+    exit 1
+  else
+    echo -e "$2 ... $G SUCCESS $N" | tee -a "$LOG_FILE"
+  fi
+}
